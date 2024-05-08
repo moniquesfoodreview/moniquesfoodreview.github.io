f403d5a94da035f1f82da0695929afa3148940fa
moniquesfoodreview <145597359+moniquesfoodreview@users.noreply.github.com>
2024-05-07 22:04:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#sub_header_underline').addEventListener('mouseover', function() {
+        document.querySelector('#sub_header_underline').style.textDecoration = "underline";
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#sub_header_underline').addEventListener('mouseover', function() {
+        document.querySelector('#sub_header_underline').style.color = 'green';
+    });
+});
